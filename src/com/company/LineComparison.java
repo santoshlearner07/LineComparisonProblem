040713c9091c09673c521606fe67ebb2eeaeeae6
santoshlearner07 <nandiyawarsantosh.0719@gmail.com>
2021-10-02 01:19:47 +0530

    Comparing two lines

--- src/com/company/LineComparison.java
+++ src/com/company/LineComparison.java
@@ -1,10 +1,21 @@
 package com.company;
+/*
+Line Comparison problem
+ */
 
 public class LineComparison {
+
     public static void main(String[] args) {
         Double firstLine;
-        int x1 = 1, x2 = 5, y1 = 1, y2 = 5; //value assigned
+        Double secondLine;
+
+        int x1 = 10, x2 = 16, y1 = 12, y2 = 14; // for first line
+        int a1 = 6, a2 = 7, b1 = 18, b2 = 9; // for second line
         firstLine = (Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2)));
-        System.out.println("The dist between two points:- " + String.format("%.2f",firstLine) );
+        secondLine = (Math.sqrt(Math.pow(a2 - a1, 2) + Math.pow(b2 - b1, 2)));
+        boolean equalLine = secondLine.equals(firstLine); // defines true or false by comparing
+        if (equalLine == false) {
+            System.out.println("First line and Second line are not Equal");
+        } else System.out.println("First line and Second line are Equal");
     }
 }
