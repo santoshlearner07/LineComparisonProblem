a50ac9adfeeff7fdcc599b30448940b037241a75
santoshlearner07 <nandiyawarsantosh.0719@gmail.com>
2021-10-02 00:31:24 +0530

    Welcome to Line Comparison Computation Program

---
+++ src/com/company/LIneComparison.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class LIneComparison {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Line Comparison Computation Program");
+    }
+}
