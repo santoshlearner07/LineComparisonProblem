5dea3c3ac7a006f4aeaf5764992d6a91e82b10eb
santoshlearner07 <nandiyawarsantosh.0719@gmail.com>
2021-10-02 11:22:43 +0530

    Arranged value properly

--- src/com/company/LineComparison.java
+++ src/com/company/LineComparison.java
@@ -4,13 +4,14 @@ Line Comparison problem
  */
 
 public class LineComparison {
+    public static int x1 = 10, x2 = 16, y1 = 12, y2 = 14; // for first line
+    public static int a1 = 6, a2 = 7, b1 = 18, b2 = 9; // for second line
+
     public static void main(String[] args) {
 
         Double firstLine;
         Double secondLine;
 
-        int x1 = 10, x2 = 16, y1 = 12, y2 = 14; // for first line
-        int a1 = 6, a2 = 7, b1 = 18, b2 = 9; // for second line
 
         firstLine = (Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2)));  // value assigned to line
         secondLine = (Math.sqrt(Math.pow(a2 - a1, 2) + Math.pow(b2 - b1, 2)));  // value assigned to line
